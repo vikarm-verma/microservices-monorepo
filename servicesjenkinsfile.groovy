f49e2b5b0ade33864b327fa3637bd8a8482a7aa7
Vikram Verma <vikram.verma@collaberatact.com>
2024-09-16 22:11:07 +0530

    adding jenkins file now after making change 3

--- servicesjenkinsfile.groovy
+++ servicesjenkinsfile.groovy
@@ -11,67 +11,67 @@ pipeline {
             steps {
                 dir('eureka-server') {
                     // Build eureka-server
-                    sh 'mvn clean package'
+                    bat 'mvn clean package'
 
                     // Build Docker image
-                    sh 'docker build -t vikdocker6785/eureka-server .'
+                    bat 'docker build -t vikdocker6785/eureka-server .'
 
                     // Push Docker image to Docker Hub
-                    sh 'docker push vikdocker6785/eureka-server'
+                    bat 'docker push vikdocker6785/eureka-server'
                 }
             }
         }
         stage('Build and Dockerize apigateway') {
             steps {
                 dir('apigateway') {
-                    sh 'mvn clean package'
-                    sh 'docker build -t vikdocker6785/apigateway .'
-                    sh 'docker push vikdocker6785/apigateway'
+                    bat 'mvn clean package'
+                    bat 'docker build -t vikdocker6785/apigateway .'
+                    bat 'docker push vikdocker6785/apigateway'
                 }
             }
         }
          stage('Build and Dockerize springsecurityjwt') {
             steps {
                 dir('springsecurityjwt') {
-                    sh 'mvn clean package'
-                    sh 'docker build -t vikdocker6785/springsecurityjwt .'
-                    sh 'docker push vikdocker6785/springsecurityjwt'
+                    bat 'mvn clean package'
+                    bat 'docker build -t vikdocker6785/springsecurityjwt .'
+                    bat 'docker push vikdocker6785/springsecurityjwt'
                 }
             }
         }
           stage('Build and Dockerize userservice') {
             steps {
                 dir('userservice') {
-                    sh 'mvn clean package'
-                    sh 'docker build -t vikdocker6785/userservice .'
-                    sh 'docker push vikdocker6785/userservice'
+                    bat 'mvn clean package'
+                    bat 'docker build -t vikdocker6785/userservice .'
+                    bat 'docker push vikdocker6785/userservice'
                 }
             }
         }
           stage('Build and Dockerize productservice') {
             steps {
                 dir('productservice') {
-                    sh 'mvn clean package'
-                    sh 'docker build -t vikdocker6785/productservice .'
-                    sh 'docker push vikdocker6785/productservice'
+                    bat 'mvn clean package'
+                    bat 'docker build -t vikdocker6785/productservice .'
+                    bat 'docker push vikdocker6785/productservice'
                 }
             }
         }
           stage('Build and Dockerize configserver') {
             steps {
                 dir('configserver') {
-                    sh 'mvn clean package'
-                    sh 'docker build -t vikdocker6785/configserver .'
-                    sh 'docker push vikdocker6785/configserver'
+                    bat 'mvn clean package'
+                    bat 'docker build -t vikdocker6785/configserver .'
+                    bat 'docker push vikdocker6785/configserver'
                 }
             }
         }
         stage('Build and Dockerize configclient') {
             steps {
                 dir('configclient') {
-                    sh 'mvn clean package'
-                    sh 'docker build -t vikdocker6785/configclient .'
-                    sh 'docker push vikdocker6785/configclient'
+                    bat 'mvn clean package'
+                    bat 'docker build -t vikdocker6785/configclient .'
+                    bat 'docker push vikdocker6785/configclient'
                 }
             }
         }
